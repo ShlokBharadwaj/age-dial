0dd605c0190a6c9bf28d9252698833dcdde599cc
ShlokBharadwaj <shlokbharadwaj7@gmail.com>
2023-10-17 18:19:16 +0530

    Add: Age input via text

--- src/components/Age.jsx
+++ src/components/Age.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const Age = () => {
-  return (
-    <div>Age</div>
-  )
-}
+    return (
+        <div className="flex items-center justify-center h-screen bg-black">
+            <div className="text-white text-center">
+                <h1 className="text-4xl mb-4">Enter Your Age</h1>
+                <input
+                    className="bg-gray-800 text-white px-4 py-2 rounded-md mb-4"
+                    type="number"
+                    placeholder="Age"
+                />
+                <br />
+                <button className="bg-gray-700 text-white px-4 py-2 rounded-md">
+                    Submit
+                </button>
+            </div>
+        </div>
+    );
+};
 
-export default Age
+export default Age;
